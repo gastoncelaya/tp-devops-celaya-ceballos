9a480d383b7380494fbeeaee4ea1e20fa800c0ae
German-Ceballos <cebagerman@gmail.com>
2025-09-03 22:35:21 -0300

    Agrego comentarios a index(archivo que levanta el server)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const app = require('./app');
-const PORT = process.env.PORT || 3000;
+const app = require('./app'); // se trae al objeto app desde el archivo app
+const PORT = process.env.PORT || 3000; // se define el puerto donde el servidor va a escuchar las peticiones. process.env.port verifica que si hay un puerto definido en las variables de entorno
 
 app.listen(PORT, () => console.log(`Servidor escuchando en puerto ${PORT}`));
