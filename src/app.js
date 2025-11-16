c550d1fcdd11e59630acebb1cc0eae52903e461a
Tonga <gcelayacompu@gmail.com>
2025-11-16 15:32:05 -0300

    agregado logs

--- src/app.js
+++ src/app.js
@@ -3,19 +3,30 @@ const app = express(); // app es el servidor express, es nuestro objeto y tiene 
 app.use(express.json()); // express.json() permite que Express lea datos en formato JSON enviados en el cuerpo de una solicitud POST o PUT.
 
 // Health check
-app.get('/health', (_req, res) => res.json({ status: 'ok', version: '1.1.3' })); //metodo que ejecuta una solicitud get y devuelve ok
+app.get('/health', (_req, res) => {
+  // LOG AGREGADO
+  console.info('Health check solicitado!'); 
+  res.json({ status: 'ok', version: '1.1.3' }); //metodo que ejecuta una solicitud get y devuelve ok
+});
 
 // CRUD simple en memoria
 let items = []; // creamos un array para cargarlo con las solicitudes
 
 // get
-app.get('/items', (_req, res) => res.json(items)); // solicitud get
+app.get('/items', (_req, res) => {
+  // LOG AGREGADO
+  console.info(`GET /items - Devolviendo ${items.length} items`);
+  res.json(items); // solicitud get
+});
 
 // create
 app.post('/items', (req, res) => {
   const { name } = req.body;
   const item = { id: Date.now(), name };
   items.push(item);
+  
+  // LOG AGREGADO
+  console.info('POST /items - Nuevo item creado:', item);
   res.status(201).json(item);
 }); // solicitud post
 
@@ -26,16 +37,24 @@ app.put('/items/:id', (req, res) => {
   const index = items.findIndex(i => i.id === id);
 
   if (index === -1) {
+    // LOG AGREGADO
+    console.warn(`PUT /items - Item no encontrado: ${id}`);
     return res.status(404).json({ error: 'Item not found' });
   }
 
   items[index].name = name;
+  
+  // LOG AGREGADO
+  console.info('PUT /items - Item actualizado:', items[index]);
   res.json(items[index]);
 }); // solicitud put
 
 // delete
 app.delete('/items/:id', (req, res) => {
-  items = items.filter(i => i.id !== Number(req.params.id));
+  const id = Number(req.params.id);
+  // LOG AGREGADO
+  console.info(`DELETE /items - Eliminando item con id: ${id}`);
+  items = items.filter(i => i.id !== id);
   res.sendStatus(204);
 }); 
 
